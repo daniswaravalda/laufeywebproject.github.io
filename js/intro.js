1260b919f2150dfb80b67f6cee00e69dc47483f8
daniswaravalda <144793220+daniswaravalda@users.noreply.github.com>
2025-09-24 19:49:02 +0700

    Add files via upload

---
+++ js/intro.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  // setelah animasi selesai (3s), redirect ke main
+  setTimeout(() => {
+    window.location.href = "home.html"; 
+  }, 3200);
+});
